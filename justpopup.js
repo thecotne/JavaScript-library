f3d3a3157c0e3852e858f9c8415a5c3122aae1ee
cotne nazarashvili <cotne.nazarashvili@gmail.com>
2014-10-21 21:48:13 +0400

    changed API in just-popup and it's now opens in center of screen

--- justpopup.js
+++ justpopup.js
@@ -1,6 +1,29 @@
-$('html').on('click','a[share-popup]',function(e){
+$('html').on('click','a[just-popup]',function(e){
 	e.preventDefault();
-	var newwindow = window.open(e.target.href,'','height=300,width=600');
+
+	var height = parseFloat($(e.currentTarget).attr('just-popup-height')) || 300;
+	var width = parseFloat($(e.currentTarget).attr('just-popup-width')) || 600;
+
+	var left = (screen.width/2)-(width/2);
+	var top = (screen.height/2)-(height/2);
+
+	var params =
+		 'toolbar=no, '
+		+'location=no, '
+		+'directories=no, '
+		+'status=no, '
+		+'menubar=no, '
+		+'scrollbars=no, '
+		+'resizable=no, '
+		+'copyhistory=no, '
+		+'width='+width+', '
+		+'height='+height+', '
+		+'top='+top+', '
+		+'left='+left
+	;
+
+	var newwindow = window.open(e.currentTarget.href,'',params);
+
 	if (window.focus) {
 		newwindow.focus();
 	}
