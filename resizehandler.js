41fbd8806b00c6193d5af981ee8437ef1812b70c
cotne nazarashvili <cotne.nazarashvili@gmail.com>
2014-11-07 13:02:48 +0400

    Update resizehandler.js
    added min and max values

--- resizehandler.js
+++ resizehandler.js
@@ -5,6 +5,19 @@ function resizeHandler(){
 	$('[data-innerWidth]').each(function(){
 		$(this).css('width',eval(window.innerWidth+$(this).attr('data-innerWidth')));
 	});
+
+	$('[data-min-innerHeight]').each(function(){
+		$(this).css('min-height',eval(window.innerHeight+$(this).attr('data-innerHeight')));
+	});
+	$('[data-min-innerWidth]').each(function(){
+		$(this).css('min-width',eval(window.innerWidth+$(this).attr('data-innerWidth')));
+	});
+	$('[data-max-innerHeight]').each(function(){
+		$(this).css('max-height',eval(window.innerHeight+$(this).attr('data-innerHeight')));
+	});
+	$('[data-max-innerWidth]').each(function(){
+		$(this).css('max-width',eval(window.innerWidth+$(this).attr('data-innerWidth')));
+	});
 };
 $(window).resize(resizeHandler);
 $(resizeHandler);
