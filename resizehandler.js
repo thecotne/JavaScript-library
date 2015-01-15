2fdb22989f985264ea1d7a27eec215e2b38f331a
cotne nazarashvili <cotne.nazarashvili@gmail.com>
2015-01-15 18:17:06 +0400

    add mobile device detection

--- resizehandler.js
+++ resizehandler.js
@@ -1,23 +1,35 @@
+oldOrientation = null;
+function isMobile(){
+	return typeof window.orientation != 'undefined';
+}
+function orientationChanged(){
+	return oldOrientation != window.orientation;
+}
+function saveCurrentOrientationAsOld(){
+	oldOrientation = window.orientation;
+}
 function resizeHandler(){
-	$('[data-innerHeight]').each(function(){
-		$(this).css('height',eval(window.innerHeight+$(this).attr('data-innerHeight')));
-	});
-	$('[data-innerWidth]').each(function(){
-		$(this).css('width',eval(window.innerWidth+$(this).attr('data-innerWidth')));
-	});
-
-	$('[data-min-innerHeight]').each(function(){
-		$(this).css('min-height',eval(window.innerHeight+$(this).attr('data-innerHeight')));
-	});
-	$('[data-min-innerWidth]').each(function(){
-		$(this).css('min-width',eval(window.innerWidth+$(this).attr('data-innerWidth')));
-	});
-	$('[data-max-innerHeight]').each(function(){
-		$(this).css('max-height',eval(window.innerHeight+$(this).attr('data-innerHeight')));
-	});
-	$('[data-max-innerWidth]').each(function(){
-		$(this).css('max-width',eval(window.innerWidth+$(this).attr('data-innerWidth')));
-	});
+	if ( ! isMobile() || orientationChanged()) {
+		saveCurrentOrientationAsOld();
+		$('[data-innerHeight]').each(function(){
+			$(this).css('height',eval(window.innerHeight+$(this).attr('data-innerHeight')));
+		});
+		$('[data-innerWidth]').each(function(){
+			$(this).css('width',eval(window.innerWidth+$(this).attr('data-innerWidth')));
+		});
+		$('[data-min-innerHeight]').each(function(){
+			$(this).css('min-height',eval(window.innerHeight+$(this).attr('data-innerHeight')));
+		});
+		$('[data-min-innerWidth]').each(function(){
+			$(this).css('min-width',eval(window.innerWidth+$(this).attr('data-innerWidth')));
+		});
+		$('[data-max-innerHeight]').each(function(){
+			$(this).css('max-height',eval(window.innerHeight+$(this).attr('data-innerHeight')));
+		});
+		$('[data-max-innerWidth]').each(function(){
+			$(this).css('max-width',eval(window.innerWidth+$(this).attr('data-innerWidth')));
+		});
+	};
 };
 $(window).resize(resizeHandler);
-$(resizeHandler);
+resizeHandler();
